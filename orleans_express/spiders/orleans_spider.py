37309926319a5d8e4d9fdb64ae7506c9fffb77eb
Adam Hammes <ahammes@cortexmedia.ca>
2019-03-03 13:10:32 -0500

    Scrape all tickets four months into the future
    I tweaked some settings to hopefully make the scrape really gentle on
    their servers.

--- orleans_express/spiders/orleans_spider.py
+++ orleans_express/spiders/orleans_spider.py
@@ -1,26 +1,37 @@
+import datetime
 import json
 
 import scrapy
 
 AJAX_URL = "https://www.orleansexpress.com/wp-admin/admin-ajax.php?lang=en"
-HOME_URL = "https://www.orleansexpress.com/en/select-the-outbound-trip/"
+NUM_DAYS_TO_SCRAPE = 120
+
+
+def format_date(date):
+    return datetime.datetime.strftime(date, "%Y-%m-%d")
 
 
 class OrleansSpider(scrapy.Spider):
     name = "orleans"
 
     def start_requests(self):
-        outbound_date = "2019/03/09"
-        form_data = {
-            "action": "action_get_complete_schedules",
-            "o": "QUE",
-            "d": "MTL",
-            "carriers": "oexp",
-            "outboundDate": outbound_date,
-            "returnDate": "",
-        }
+        today = datetime.datetime.now()
 
-        yield scrapy.http.FormRequest(url=AJAX_URL, formdata=form_data)
+        for num_days_in_future in range(0, NUM_DAYS_TO_SCRAPE, 3):
+            # The API gives results in 3 day windows centered around the
+            # requested date.
+            date = today + datetime.timedelta(days=num_days_in_future + 1)
+
+            form_data = {
+                "action": "action_get_complete_schedules",
+                "o": "QUE",
+                "d": "MTL",
+                "carriers": "oexp",
+                "outboundDate": format_date(date),
+                "returnDate": "",
+            }
+
+            yield scrapy.http.FormRequest(url=AJAX_URL, formdata=form_data)
 
     def parse(self, response):
         data = json.loads(response.text)
